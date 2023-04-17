1e48ce1fafed6fa62351fcbabe73a98df3839b3b
Nechaev <diman00nechaev@gmail.com>
2023-04-17 12:57:23 +0700

    123
    123

---
+++ NechaevPM4/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NechaevPM4
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Name_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (Name.Text.Length < 3 || Name.Text.Length > 30)
+            {
+                Name.Background = Brushes.Red;
+            }
+           
+
+        }
+
+        private void FirstName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+                if (FirstName.Text.Length < 2 || FirstName.Text.Length > 35) 
+                {
+                FirstName.Background = Brushes.Red;
+                }
+                
+            
+
+        }
+
+        private void Old_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+            if (Old.Text.Length > 2)
+            {
+                FirstName.Background = Brushes.Red;
+            }
+
+
+
+        }
+
+
+        private void Birthday_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+  }
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Name_WithLessThan3Characters_ReturnsFalse()
+        {
+            // Arrange
+            string name = "ab";
+
+            // Act
+            bool result = IsNameValid(name);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+
+
+
+        public void Name_WithMoreThan15Characters_ReturnsFalse()
+        {
+            // Arrange
+            string name = "abcdefghijklmnop";
+
+            // Act
+            bool result = IsNameValid(name);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+
+
+        public void Name_With3To15Characters_ReturnsTrue()
+        {
+            // Arrange
+            string name1 = "abc";
+            string name2 = "abcdefghijklmno";
+
+            // Act
+            bool result1 = IsNameValid(name1);
+            bool result2 = IsNameValid(name2);
+
+            // Assert
+            Assert.IsTrue(result1);
+            Assert.IsTrue(result2);
+        }
+
+        private bool IsNameValid(string name)
+        {
+           
+            if (name.Length < 3 || name.Length > 15)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+
+
+    }
+}
